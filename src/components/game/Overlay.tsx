089999b65e844e93ed4db6f2275d22a466e41e70
MatthewDHarrison <madoha5812@gmail.com>
2024-08-29 19:21:14 +1000

    feat: add simple losing health anim

--- src/components/game/Overlay.tsx
+++ src/components/game/Overlay.tsx
@@ -1,6 +1,6 @@
 import { Favorite, FavoriteBorder, Toll } from "@mui/icons-material";
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ICharacter } from "../../game/character/character";
 
 interface IOverlayProps {
@@ -9,9 +9,24 @@ interface IOverlayProps {
 
 export const Overlay = ({ character }: IOverlayProps) => {
   const name = character.name;
-  const health = character.hp;
+  const [currentHealth, setCurrentHealth] = useState(character.hp);
   const maxHealth = 30;
   const location = "Rotblack Dungeon";
+  const [heartSize, setHeartSize] = useState("medium");
+  const [heartColor, setHeartColor] = useState("white");
+
+  useEffect(() => {
+    setCurrentHealth(character.hp);
+    if (character.hp < currentHealth) {
+      // change heart size and color for 1 second
+      setHeartColor("red");
+      setHeartSize("large");
+      setTimeout(() => {
+        setHeartColor("white");
+        setHeartSize("medium");
+      }, 100);
+    }
+  }, [character]);
 
   return (
     <Box zIndex={1} position="absolute" height="100vh" width="100%">
@@ -41,24 +56,44 @@ export const Overlay = ({ character }: IOverlayProps) => {
             <Box
               display="flex"
               alignItems="center"
-              justifyContent="center"
+              justifyContent="space-between"
               gap={1}
               flexDirection="row"
+              height="6-px"
+              width="120px"
             >
-              <FavoriteBorder />
-              <Typography variant="game" fontSize={20}>
-                {health}/{maxHealth}
+              <Box height="20px" sx={{ ml: 2 }}>
+                <FavoriteBorder
+                  fontSize={heartSize}
+                  htmlColor={heartColor}
+                  sx={{
+                    transition: "color 0.1s, size 0.1s",
+                    transform: "translate(-50%, -50%)",
+                  }}
+                />
+              </Box>
+              <Typography variant="game" fontSize={20} sx={{ mb: 2.3 }}>
+                {currentHealth}/{maxHealth}
               </Typography>
             </Box>
             <Box
               display="flex"
               alignItems="center"
-              justifyContent="center"
+              justifyContent="space-between"
               gap={1}
               flexDirection="row"
+              height="6-px"
+              width="120px"
             >
-              <Toll />
-              <Typography variant="game" fontSize={20}>
+              <Box height="20px" sx={{ ml: 2 }}>
+                <Toll
+                  sx={{
+                    transition: "color 0.1s, size 0.1s",
+                    transform: "translate(-50%, -50%)",
+                  }}
+                />
+              </Box>
+              <Typography variant="game" fontSize={20} sx={{ mb: 2 }}>
                 {character.coin}
               </Typography>
             </Box>
